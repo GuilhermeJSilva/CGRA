14b72f5b3a0f569c7902457ad2bb5d53977d4cda
Guilherme Silva <guilhermejosesilva@gmail.com>
2018-05-24 15:40:52 +0100

    Final normals??

--- Tp6/tp6/MyTerrain.js
+++ Tp6/tp6/MyTerrain.js
@@ -27,21 +27,21 @@ class MyTerrain extends Plane {
             if (tmp[1] == 0) {
               normal[1] = normal[1] + 1;
             } else if (tmp[0] / Math.abs(tmp[0]) == 1) {
-              normal[1] = normal[1] + Math.abs(tmp[0] == 0 ? 0 : 1 / tmp[0]);
-              normal[0] = normal[0] - Math.abs(tmp[1] == 0 ? 0 : 1 / tmp[1]);
+              normal[1] = normal[1] + tmp[0] == 0 ? 0 : 1 / tmp[0];
+              normal[0] = normal[0] - tmp[1] == 0 ? 0 : 1 / tmp[1];
             } else {
-              normal[1] = normal[1] - Math.abs(tmp[0] == 0 ? 0 : 1 / tmp[0]);
-              normal[0] = normal[0] + Math.abs(tmp[1] == 0 ? 0 : 1 / tmp[1]);
+              normal[1] = normal[1] - tmp[0] == 0 ? 0 : 1 / tmp[0];
+              normal[0] = normal[0] + tmp[1] == 0 ? 0 : 1 / tmp[1];
             }
           } else if (j % 2 == 0) {
             if (tmp[1] == 0) {
               normal[1] = normal[1] + 1;
             } else if (tmp[2] / Math.abs(tmp[2]) == 1) {
-              normal[1] = normal[1] + Math.abs(tmp[2] == 0 ? 0 : 1 / tmp[2]);
-              normal[2] = normal[2] - Math.abs(tmp[1] == 0 ? 0 : 1 / tmp[1]);
+              normal[1] = normal[1] + tmp[2] == 0 ? 0 : 1 / tmp[2];
+              normal[2] = normal[2] - tmp[1] == 0 ? 0 : 1 / tmp[1];
             } else {
-              normal[1] = normal[1] - Math.abs(tmp[2] == 0 ? 0 : 1 / tmp[2]);
-              normal[2] = normal[2] + Math.abs(tmp[1] == 0 ? 0 : 1 / tmp[1]);
+              normal[1] = normal[1] - tmp[2] == 0 ? 0 : 1 / tmp[2];
+              normal[2] = normal[2] + tmp[1] == 0 ? 0 : 1 / tmp[1];
             }
           }
         }
